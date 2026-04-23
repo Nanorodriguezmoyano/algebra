74dd4eed7c897bb17f6cf801a3c3bab205009ebf
Nano <narodriguez@frd.utn.edu.ar>
2026-04-22 22:18:44 -0300

    Inicializar e imprimir matriz

--- matrix.c
+++ matrix.c
@@ -3,43 +3,37 @@
 
 
 typedef struct Matrix{
-	int column;
+	int col;
 	int row;
 	float **data;
 }Matrix;
-Matrix *init_matrix(int row, int column);
-int main(int argv, char *arcgv[])
+Matrix *init_matrix(int row, int col);
+void print_matrix(Matrix* matrix);
+Matrix *set_elem(int row, int col, c/
+int main(int argc, char *argv[])
 {
-	float **matriz;
-	int column = 3;
-	int row = 2;
-	for (int i = 0; i<row; i++)
-	{
-		*matriz = calloc(column, sizeof(float));
-		*matriz ++;
-	}
-	for (int i = 0; i<row; i++)
-	{
-		for (int j = 0; j<column; j++) printf("%.2f", **(matriz++));
-		printf("\n");
-		*matriz ++;
-	}
-
+	Matrix * matrix;
+	matrix = init_matrix(atoi(argv[1]),atoi(argv[2]));
+	print_matrix(matrix);
 }
-/*
-Matrix *init_matrix(int row, int column)
+Matrix *init_matrix(int row, int col)
 {
 	Matrix *matrix;
-	matrix->column = column;
+	matrix= calloc(1, sizeof(Matrix));
+	matrix->col = col;
 	matrix->row = row;
+	matrix->data = calloc(matrix->row, sizeof(float *));
 	for (int i = 0; i < row; i++)
 	{
-		*(matrix->data++) = calloc(column, sizeof(float));
-	}
-	for (int i = 0; i<column; i ++)
-	{
-		matrix->data[i] = calloc
+		matrix->data[i] = calloc(col, sizeof(float));
 	}
 	return matrix; 
 }
-*/
+void print_matrix(Matrix* matrix)
+{
+	for(int i = 0; i<matrix->row; i++ )
+	{
+		for(int j=0; j<matrix->col; j++) printf("%.0f ", matrix->data[i][j]);
+		printf("\n");
+	}
+}
